5aa91f8f95da4fef5eb8774c5d860d8655c24ec6
Lucky Mishra <luckymishra4097gmail.com>
2026-05-22 22:34:37 +0530

    Production deployment setup

--- server.js
+++ server.js
@@ -7,33 +7,32 @@ const ticketRoutes = require("./routes/ticketRoutes");
 const socketHandler = require("./socket/socketHandler");
 
 const app = express();
+
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "https://syncdesk.vercel.app"
+    ],
     methods: ["GET", "POST", "PUT"]
   }
 });
 
 app.use(cors());
+
 app.use(express.json());
 
-/*
-  REST APIs
-*/
 app.use("/api/tickets", ticketRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Helpdesk Backend Running");
+  res.send("SyncDesk Backend Running");
 });
 
-/*
-  Initialize Socket.io
-*/
 socketHandler(io);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
